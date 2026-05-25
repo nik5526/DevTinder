e6bc11ee751939d18345d0bbf959dcad3086e1a9
Nikhil Raj <nikhilrajy909@gmail.com>
2026-05-25 20:37:47 +0530

    middleware and next()

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello" , (req,res)=>{
-    res.send("Hello Hello Hello Nikhil")
+app.use("/admin",(req,res,next)=>{
+    const tokens = "xyz";
+    const Authentication = tokens === "xyz";
+    if(!Authentication){
+        res.status(401).send("Unautherized request");
+    }
+    else{
+        next();
+    }
 });
 
-app.use((req,res)=>{
-    res.send("Hello Hittu bhai....")
+app.get("/admin/MainData",(req,res)=>{
+    res.send("data is sent");
 });
+
+app.get("/admin/deleteData",(req,res)=>{
+    res.send("Data is deleted");
+});
+
+
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
